4e47865d8446855020db03e667f3fc575ea8474f
Kamil Stecyk <kamils987@interia.eu>
2023-01-16 23:56:17 +0100

    add backend for displaying dish details

--- restaurant-project/src/app/components/dish-details/dish-details.component.ts
+++ restaurant-project/src/app/components/dish-details/dish-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DishService } from 'src/app/services/dish.service';
+import { map } from 'rxjs';
+import { Dish } from 'src/app/models/dish.model';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-dish-details',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./dish-details.component.scss']
 })
 export class DishDetailsComponent {
+  dish: Dish;
+  dish_key: string;
+
+  constructor(private dishService: DishService, private activatedRoute: ActivatedRoute)
+  {
+    this.dish_key = this.activatedRoute.snapshot.params['key'];
+    this.getDish();
+  }
+
+  getDish(): void {
+
+    this.dishService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      
+        for(let dish_record of data)
+        {
+          if(dish_record.key == this.dish_key)
+          {
+            this.dish = dish_record as Dish;
+            console.log("Dish details: ");
+            console.log(this.dish);
+          }
+        }
+
+      });
+  };
 
 }

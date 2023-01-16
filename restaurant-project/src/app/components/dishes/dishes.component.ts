630711d9f20fdefe2f102b31cd7eb2285c1eec2c
Kamil Stecyk <kamils987@interia.eu>
2023-01-16 21:35:42 +0100

    add backend for displaying dishes

--- restaurant-project/src/app/components/dishes/dishes.component.ts
+++ restaurant-project/src/app/components/dishes/dishes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Dish } from '../../models/dish.model';
+import { DishService } from '../../services/dish.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dishes',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./dishes.component.scss']
 })
 export class DishesComponent {
+  dishes: Dish[] = [];
+  highest_price = 0;
+  cheapest_price = 0;
+
+  constructor(private dishService: DishService)
+  {
+    this.getAllDishes();
+  }
+
+  getAllDishes(): void {
+
+    this.dishService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      
+        this.dishes = data as Dish[];
+
+        this.highest_price = Math.max(...this.dishes.map(dish => Number(dish.price)));
+        this.cheapest_price = Math.min(...this.dishes.map(dish => Number(dish.price)));
+
+        console.log("Retreived data:");
+        console.log(this.dishes);
+
+      });
+  };
 
 }

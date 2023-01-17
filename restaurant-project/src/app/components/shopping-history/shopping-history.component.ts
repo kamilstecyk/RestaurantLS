6eec6e96901bd3b6d9bc39b31ba6da56490721ed
Kamil Stecyk <kamils987@interia.eu>
2023-01-17 03:31:21 +0100

    add history of orders backend

--- restaurant-project/src/app/components/shopping-history/shopping-history.component.ts
+++ restaurant-project/src/app/components/shopping-history/shopping-history.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HistoryRecord } from 'src/app/services/buy.service';
+import { BuyService } from 'src/app/services/buy.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-history',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-history.component.scss']
 })
 export class ShoppingHistoryComponent {
+  history_records: HistoryRecord[] = [];
+  history_records_subscription: any;
 
+  constructor(private buyService: BuyService)
+  {
+    this.getAllHistoryRecords();
+  }
+
+  getAllHistoryRecords()
+  {
+    this.history_records_subscription = this.buyService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+        this.history_records = data;
+        console.log(data);
+      }
+    )
+  }
+
+  getHistoryRecordFullPrice(record: HistoryRecord): string 
+  {
+    if(record.order_record?.amount_to_order)
+      return (record.order_record?.amount_to_order * Number(record.order_record?.price_per_dish)).toFixed(2);
+    
+    return "0.00";
+  }
+
+  ngOnDestroy(): void {
+      if(this.history_records_subscription)
+        this.history_records_subscription.unsubscribe();
+  }
 }
